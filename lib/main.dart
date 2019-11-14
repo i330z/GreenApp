56ca64d309af0d8cf6232d0450ba5dae4d44cb65
i330z <izzazahmed134@gmail.com>
2019-11-14 15:04:22 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -209,39 +209,140 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: greenColor,
-      body: Container(
-        padding: EdgeInsets.all(32.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Greenary nyc',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 1.1,
-                fontSize: 22.0,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Container(
-              width: 200,
-              child: Text(
-                'Product Overview',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 38.0,
-                  fontWeight: FontWeight.bold,
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(32.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Greenary nyc',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 1.1,
+                    fontSize: 22.0,
+                  ),
                 ),
+                SizedBox(height: 30.0),
+                Container(
+                  width: 200.0,
+                  child: Text(
+                    'Product Overview',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 38.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                itemRow(Icons.star, 'water', 'every 7 days'),
+                SizedBox(height: 22.0),
+                itemRow(Icons.ac_unit, 'Humidity', 'up to 82%'),
+                SizedBox(height: 22.0),
+                itemRow(Icons.straighten, 'Size', '38" - 48"tdll'),
+              ],
+            ),
+          ),
+          SizedBox(height: 12.0,),
+          Padding(
+            padding: const EdgeInsets.only(left:14.0),
+            child: Container(
+              height: 60.0,
+              decoration: BoxDecoration(
+                  color: darkGreenColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(32.0),
+                    bottomLeft: Radius.circular(32.0),
+                  )
+              ),
+              child: Row(
+                children: <Widget>[
+                  SizedBox(width: 20.0),
+                  Icon(Icons.add, color: Colors.white, size:24.0,),
+                  SizedBox(width: 40.0),
+                  Text(
+                    'Delivery Information',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 30.0),
-            itemRow(Icons.star, 'water', 'every 7 days'),
-            SizedBox(height: 22.0),
-            itemRow(Icons.ac_unit, 'Humidity', 'up to 82%'),
-            SizedBox(height: 22.0),
-            itemRow(Icons.straighten, 'Size', '38" - 48"tdll'),
-          ],
-        ),
-      )
+          ),
+          SizedBox(height: 12.0),
+          Padding(
+            padding: const EdgeInsets.only(left:14.0),
+            child: Container(
+              height: 60.0,
+              decoration: BoxDecoration(
+                  color: darkGreenColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(32.0),
+                    bottomLeft: Radius.circular(32.0),
+                  )
+              ),
+              child: Row(
+                children: <Widget>[
+                  SizedBox(width: 20.0),
+                  Icon(Icons.add, color: Colors.white, size:24.0,),
+                  SizedBox(width: 40.0),
+                  Text(
+                    'Return Policy',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Spacer(),
+          Container(
+            height: 80.0,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width/2,
+                  child: Icon(
+                    Icons.arrow_drop_down,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  height: 80.0,
+                  width: MediaQuery.of(context).size.width/2,
+                  decoration: BoxDecoration(
+                    color: Color(0xff2c2731),
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(48.0)),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.add_shopping_cart,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 6.0),
+                      Text(
+                        'add to cart',
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+
     );
   }
 }
